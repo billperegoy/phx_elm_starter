2e037af0a3848ec0be1d82e65bd7762721f1e1dd
Bill Peregoy <bill@gnar.dog>
2018-12-08 23:36:07 -0500

    Cleaned up app
    * No longer needed a div for app since we take over whole browser

--- assets/elm/src/index.js
+++ assets/elm/src/index.js
@@ -2,8 +2,6 @@ import './main.css';
 import { Elm } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
-Elm.Main.init({
-  node: document.getElementById('root')
-});
+Elm.Main.init();
 
 registerServiceWorker();
